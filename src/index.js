17d8744215dedfb63d61939f7dae301f1b69d8f4
William ☕️ <113069637+williamx0@users.noreply.github.com>
2024-06-14 19:50:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CookiesProvider } from 'react-cookie'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,6 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
     <App />
   </React.StrictMode>,
 )
